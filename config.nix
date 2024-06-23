47a212bcf7a8866741b934d45ac91e51f1db7c2c
Brandon Hops <hops.brandon@gmail.com>
2024-06-23 16:53:59 -0400

    initial commit

---
+++ config.nix
@@ -0,0 +1,15 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "bhops-tools";
+      paths = [
+        bash-completion
+        neovim
+        go
+        ripgrep
+        fzf
+        lazygit
+      ];
+    };
+  };
+}
