8820b168be5a453435952587875273415d24401c
Brandon Hops <hops.brandon@gmail.com>
2024-06-23 19:58:22 -0400

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
