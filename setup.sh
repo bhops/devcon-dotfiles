29bf6cfbf6d4a00ea1b05799555e75bfa280f07a
Brandon Hops <hops.brandon@gmail.com>
2024-06-24 14:22:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
+# Workaround SSL proxy wonkiness
+sudo /nix/var/nix/profiles/default/bin/nix-channel --update nixpkgs
+
 # install Nix packages from config.nix
 nix-env -iA nixpkgs.myPackages
